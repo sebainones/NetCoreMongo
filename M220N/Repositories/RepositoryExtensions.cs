2a2e2baa095c474ac07c331431b4440241184792
Sebastián Iñones <sebainones@gmail.com>
2020-10-07 20:06:53 +0200

    I prefer my way to set the Max Connection Pool for Mongo

--- M220N/Repositories/RepositoryExtensions.cs
+++ M220N/Repositories/RepositoryExtensions.cs
@@ -8,10 +8,18 @@ namespace M220N.Repositories
     {
         public static void RegisterMongoDbRepositories(this IServiceCollection servicesBuilder)
         {
+            //https://docs.mongodb.com/manual/reference/connection-string/
+
             servicesBuilder.AddSingleton<IMongoClient, MongoClient>(s =>
             {
+                int maxConnections = int.Parse(s.GetRequiredService<IConfiguration>()["MongoMaxConnections"]);
                 var uri = s.GetRequiredService<IConfiguration>()["MongoUri"];
-                return new MongoClient(uri);
+
+                var mongoClientSettings = MongoClientSettings.FromConnectionString(uri);
+                mongoClientSettings.MaxConnectionPoolSize = maxConnections;
+
+                return new MongoClient(mongoClientSettings);
+                //return new MongoClient(uri);
             });
             servicesBuilder.AddSingleton<MoviesRepository>();
             servicesBuilder.AddSingleton<UsersRepository>();
